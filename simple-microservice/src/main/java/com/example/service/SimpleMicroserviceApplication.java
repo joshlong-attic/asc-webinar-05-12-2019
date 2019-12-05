4e2a0c9eb54409c99b2736b77bfe8f9f6b3eb7b4
Josh Long <josh@joshlong.com>
2019-12-05 22:48:45 +1100

    up

--- simple-microservice/src/main/java/com/example/service/SimpleMicroserviceApplication.java
+++ simple-microservice/src/main/java/com/example/service/SimpleMicroserviceApplication.java
@@ -3,61 +3,63 @@ package com.example.service;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-import java.util.Map;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
-@Log4j2
 @SpringBootApplication
 public class SimpleMicroserviceApplication {
-
-  @Bean
-  RouterFunction<ServerResponse> routes(@Value("${spring.data.mongodb.uri:}") String mongoDbUri,
-      @Value("${application.message}") String message, CustomerRepository customerRepository) {
-
-    log.info("mongoDbUri: " + mongoDbUri);
-
-    return route().GET("/customers", req -> {
-      log.info("returning all the " + Customer.class.getName() + " instances.");
-      return ok().body(customerRepository.findAll(), Customer.class);
-    }).GET("/hello", r -> ok().bodyValue(Map.of("greeting", message))).build();
-  }
-
-  @Bean
-  ApplicationListener<ApplicationReadyEvent> ready(CustomerRepository repository) {
-    return event -> repository.deleteAll()
-        .thenMany(Flux.just("A", "B", "C").map(name -> new Customer(null, name)).flatMap(repository::save))
-        .thenMany(repository.findAll()).subscribe(log::info);
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(SimpleMicroserviceApplication.class, args);
   }
+
+  @Bean
+  RouterFunction<ServerResponse> routes(
+      CustomerRepository repository,
+      DiscoveryClient dc,
+      @Value("${spring.data.mongodb.uri:}") String mongoDbUri,
+      @Value("${application.message:}") String message) {
+    return route()
+        .GET("/message", r -> ok().bodyValue(message))
+        .GET("/mongo", r -> ok().bodyValue(mongoDbUri))
+        .GET("/clients", r -> ok().bodyValue(this.hostsAndPorts(dc)))
+        .GET("/customers", request -> ok().body(repository.findAll(), Customer.class))
+        .build();
+  }
+
+  private Collection<String> hostsAndPorts(DiscoveryClient dc) {
+    return dc.getInstances("simple-microservice")
+        .stream()
+        .map(si -> si.getUri().toString())
+        .collect(Collectors.toList());
+  }
+
 }
 
 interface CustomerRepository extends ReactiveCrudRepository<Customer, String> {
 }
 
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
 class Customer {
-
   @Id
   private String id;
   private String name;
